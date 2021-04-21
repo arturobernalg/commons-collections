e289efb9cced10839be197fd9defb19b6d9d329c
Arturo Bernal <arturobernalg@gmail.com>
2021-04-21 06:45:04 +0200

    COLLECTIONS-790 - Simplify conditional expression

--- src/main/java/org/apache/commons/collections4/comparators/FixedOrderComparator.java
+++ src/main/java/org/apache/commons/collections4/comparators/FixedOrderComparator.java
@@ -282,8 +282,7 @@ public class FixedOrderComparator<T> implements Comparator<T>, Serializable {
                    (null == unknownObjectBehavior ? null == comp.unknownObjectBehavior :
                         unknownObjectBehavior == comp.unknownObjectBehavior &&
                         counter == comp.counter &&
-                        isLocked == comp.isLocked &&
-                        unknownObjectBehavior == comp.unknownObjectBehavior);
+                        isLocked == comp.isLocked);
         }
         return false;
     }
